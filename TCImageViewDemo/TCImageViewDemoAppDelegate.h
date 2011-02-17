7d3275a68cce831c1a82bc5882da826e88b5a7ab
Toto Tvalavadze <Toto@Totos-iMac.local>
2011-02-17 15:22:12 +0400

    Initial Commit

---
+++ TCImageViewDemo/TCImageViewDemoAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  TCImageViewDemoAppDelegate.h
+//  TCImageViewDemo
+//
+//  Created by Toto Tvalavadze on 2/17/11.
+//  Copyright 2011 63BITS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCImageViewDemoAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ TCImageViewDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TCImageViewDemo
+//
+//  Created by Toto Tvalavadze on 2/17/11.
+//  Copyright 2011 63BITS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
